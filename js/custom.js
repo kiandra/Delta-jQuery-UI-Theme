866f73525867b066af5e44d0d70307f95ec93bfe
Tait Brown <taitbrown@gmail.com>
2013-01-08 22:01:24 +1100

    should close issue #2 - radio button active state, auto complete, datepicker icon hover fixed, progress bar styling

--- js/custom.js
+++ js/custom.js
@@ -85,4 +85,39 @@ $(function() {
 		});
 	});
 
+	// AUTOCOMPLETE EXAMPLE
+	var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $(".autocomplete").autocomplete({
+        source: availableTags
+    });
+
+
+    // PROGRESS BAR EXAMPLE
+    $("#progressbar").progressbar({
+        value: 37
+    }).width(300);
+
 });
